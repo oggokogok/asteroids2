4a429fff7c5693889210e3cde641a26cc4f55a71
oggokogok <tsm9041@gmail.com>
2026-01-01 11:24:50 -0600

    got pygame working

--- asteroids-v2/main.py
+++ asteroids-v2/main.py
@@ -1,8 +1,10 @@
 import pygame
-
+version = pygame.version.ver
 def main():
-    print("Starting Asteroids")
+    print(f'Starting Asteroids with pygame version: {version}')
+    # print(version)
 
 
 if __name__ == "__main__":
     main()
+#Continue https://www.boot.dev/lessons/268bb0d0-3e63-4218-aacc-cba3247a1af5
