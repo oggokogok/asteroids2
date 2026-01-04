f67f2e7cbd08fbcf4cb915f103a6a8c2fedda7a1
oggokogok <tsm9041@gmail.com>
2026-01-04 16:27:35 -0600

    Updating  main for gameloop

--- asteroids-v2/main.py
+++ asteroids-v2/main.py
@@ -1,10 +1,32 @@
 import pygame
 import constants
+from logger import log_state
 
 version = pygame.version.ver
 width = constants.SCREEN_WIDTH
 height = constants.SCREEN_HEIGHT
+
 def main():
+    #initializing pygame
+    pygame.init()
+
+    #creating display area
+    screen = pygame.display.set_mode((width,height))
+
+    while True:
+        #Calling LogState
+        log_state()
+
+        #allowing game to close
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+        #Filling screen with black
+        screen.fill("black")
+
+        #flip refreshes screen
+        pygame.display.flip()
     print(f'Starting Asteroids with pygame version: {version}')
     print(f'Screen width: {width}')
     print(f'Screen height: {height}')
